2cca22c0a1a3369f8f7cb02ba7d06b80852f615d
jeysri123 <jeysriviswanathan@gmail.com>
2026-04-06 15:57:18 +0530

    new

---
+++ temp.java
@@ -0,0 +1,9 @@
+public class temp {
+    public static void main(String[] args){
+        double temp=26.6;
+        short change=0;
+        temp=change;
+        System.out.println(temp);
+        System.out.println(change);
+    }
+}
