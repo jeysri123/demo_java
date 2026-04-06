3501a8f955b4c438235c7367b5aa32a283b66db4
jeysri123 <jeysriviswanathan@gmail.com>
2026-04-06 16:04:27 +0530

    new

---
+++ fuel.java
@@ -0,0 +1,9 @@
+public class fuel {
+    public static void main(String[] args){
+        int distance=50;
+        int milage=45;
+        int fuel=100;
+        int total_cost=(distance/milage)*fuel;
+        System.out.println(total_cost);
+    }
+}
